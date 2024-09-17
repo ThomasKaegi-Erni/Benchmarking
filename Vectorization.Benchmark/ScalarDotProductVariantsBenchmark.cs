d50b66a5809ff7d1a8170fcce078417ca0bca9d4
Thomas Kägi <thomas.kaegi.tk1@contractors.roche.com>
2024-09-17 10:43:01 +0200

    Add env-parameterized runs.

--- Vectorization.Benchmark/ScalarDotProductVariantsBenchmark.cs
+++ Vectorization.Benchmark/ScalarDotProductVariantsBenchmark.cs
@@ -5,32 +5,32 @@ namespace Vectorization.Benchmark;
 // Do not seal benchmark classes. Benchmark.Net subclasses them...
 public class ScalarDotProductVariantsBenchmark
 {
-    private MyVector left, right;
+  private MyVector left, right;
 
-    [Params(3, 12, 128)]
-    public Int32 Size { get; set; }
+  [Params(3, 12, 128)]
+  public Int32 Size { get; set; }
 
-    [GlobalSetup]
-    public void Setup()
-    {
-        this.left = new MyVector(i => i, Size);
-        this.right = new MyVector(i => 1f / i, Size);
-    }
+  [GlobalSetup]
+  public void Setup()
+  {
+    this.left = new MyVector(i => i, Size);
+    this.right = new MyVector(i => 1f / i, Size);
+  }
 
-    [Benchmark(Baseline = true)]
-    public Single Naive() => DotProduct.Scalar(this.left, this.right);
+  [Benchmark(Baseline = true)]
+  public Single Naive() => DotProduct.Scalar(this.left, this.right);
 
-    [Benchmark]
-    public Single Generic() => DotProduct.GenericScalar<Single>(this.left, this.right);
+  [Benchmark]
+  public Single Generic() => DotProduct.GenericScalar<Single>(this.left, this.right);
 
-    [Benchmark]
-    public Single Recursive() => DotProduct.RecursiveScalar(this.left, this.right);
+  [Benchmark]
+  public Single Recursive() => DotProduct.RecursiveScalar(this.left, this.right);
 
-    [Benchmark]
-    public Single Unrolled() => DotProduct.UnrolledScalar(this.left, this.right);
+  [Benchmark]
+  public Single Unrolled() => DotProduct.UnrolledScalar(this.left, this.right);
 
-    [Benchmark]
-    public Single FusedMultiplyAdd() => DotProduct.FusedScalar(this.left, this.right);
+  [Benchmark]
+  public Single FusedMultiplyAdd() => DotProduct.FusedScalar(this.left, this.right);
 }
 
 /* Summary
@@ -62,3 +62,66 @@ BenchmarkDotNet v0.14.0, Windows 11 (10.0.22621.4037/22H2/2022Update/SunValley2)
 | Unrolled         | 128  |  52.081 ns | 0.7258 ns | 0.6789 ns |  0.87 |    0.01 |
 | FusedMultiplyAdd | 128  |  76.689 ns | 0.7915 ns | 0.7404 ns |  1.29 |    0.02 |
 Summary */
+
+/* The same with varying SIMD capabilities (indicated by the 'Job' column)
+
+BenchmarkDotNet v0.14.0, Windows 11 (10.0.22621.4037/22H2/2022Update/SunValley2)
+13th Gen Intel Core i7-13850HX, 1 CPU, 28 logical and 20 physical cores
+.NET SDK 8.0.302
+  [Host]    : .NET 8.0.6 (8.0.624.26715), X64 RyuJIT AVX2
+  Scalar    : .NET 8.0.6 (8.0.624.26715), X64 RyuJIT
+  Vector128 : .NET 8.0.6 (8.0.624.26715), X64 RyuJIT AVX
+  Vector256 : .NET 8.0.6 (8.0.624.26715), X64 RyuJIT AVX2
+
+IterationTime=250ms  MaxIterationCount=20  WarmupCount=3  
+
+| Method           | Job       | Size | Mean       | StdDev     | Ratio | Code Size |
+|----------------- |---------- |----- |-----------:|-----------:|------:|----------:|
+| Naive            | Scalar    | 3    |   1.735 ns |  0.0832 ns |  1.00 |     209 B |
+| Generic          | Scalar    | 3    |   1.679 ns |  0.0669 ns |  0.97 |     209 B |
+| Recursive        | Scalar    | 3    |   9.548 ns |  0.1087 ns |  5.51 |     579 B |
+| Unrolled         | Scalar    | 3    |   3.468 ns |  0.0952 ns |  2.00 |     432 B |
+| FusedMultiplyAdd | Scalar    | 3    |   8.942 ns |  0.1687 ns |  5.16 |     251 B |
+| Naive            | Vector128 | 3    |   2.869 ns |  0.0401 ns |  1.66 |     212 B |
+| Generic          | Vector128 | 3    |   2.907 ns |  0.0467 ns |  1.68 |     212 B |
+| Recursive        | Vector128 | 3    |   9.378 ns |  0.2728 ns |  5.42 |     591 B |
+| Unrolled         | Vector128 | 3    |   4.853 ns |  1.3701 ns |  2.80 |     424 B |
+| FusedMultiplyAdd | Vector128 | 3    |   2.159 ns |  0.2159 ns |  1.25 |     217 B |
+| Naive            | Vector256 | 3    |   2.884 ns |  0.0254 ns |  1.67 |     212 B |
+| Generic          | Vector256 | 3    |   3.399 ns |  0.7822 ns |  1.96 |     212 B |
+| Recursive        | Vector256 | 3    |   9.622 ns |  0.2642 ns |  5.56 |     591 B |
+| Unrolled         | Vector256 | 3    |   4.768 ns |  1.5139 ns |  2.75 |     424 B |
+| FusedMultiplyAdd | Vector256 | 3    |   2.658 ns |  1.0403 ns |  1.53 |     217 B |
+|                  |           |      |            |            |       |           |
+| Naive            | Scalar    | 12   |   5.104 ns |  0.1546 ns |  1.00 |     209 B |
+| Generic          | Scalar    | 12   |   4.976 ns |  0.1965 ns |  0.98 |     209 B |
+| Recursive        | Scalar    | 12   |  49.323 ns |  1.1079 ns |  9.67 |     563 B |
+| Unrolled         | Scalar    | 12   |   5.079 ns |  0.1257 ns |  1.00 |     421 B |
+| FusedMultiplyAdd | Scalar    | 12   |  46.028 ns |  0.9601 ns |  9.03 |     251 B |
+| Naive            | Vector128 | 12   |   6.770 ns |  0.1866 ns |  1.33 |     212 B |
+| Generic          | Vector128 | 12   |   6.731 ns |  0.1709 ns |  1.32 |     212 B |
+| Recursive        | Vector128 | 12   |  49.763 ns |  1.2320 ns |  9.76 |     575 B |
+| Unrolled         | Vector128 | 12   |   5.515 ns |  0.1595 ns |  1.08 |     409 B |
+| FusedMultiplyAdd | Vector128 | 12   |   5.270 ns |  0.1194 ns |  1.03 |     217 B |
+| Naive            | Vector256 | 12   |   6.632 ns |  0.1382 ns |  1.30 |     212 B |
+| Generic          | Vector256 | 12   |   6.856 ns |  0.1556 ns |  1.34 |     212 B |
+| Recursive        | Vector256 | 12   |  48.792 ns |  1.1325 ns |  9.57 |     575 B |
+| Unrolled         | Vector256 | 12   |   5.520 ns |  0.2203 ns |  1.08 |     409 B |
+| FusedMultiplyAdd | Vector256 | 12   |   5.202 ns |  0.1366 ns |  1.02 |     217 B |
+|                  |           |      |            |            |       |           |
+| Naive            | Scalar    | 128  |  49.771 ns |  0.4455 ns |  1.00 |     209 B |
+| Generic          | Scalar    | 128  |  49.723 ns |  0.5508 ns |  1.00 |     209 B |
+| Recursive        | Scalar    | 128  | 412.626 ns |  9.1671 ns |  8.29 |     572 B |
+| Unrolled         | Scalar    | 128  |  42.838 ns |  0.3795 ns |  0.86 |     421 B |
+| FusedMultiplyAdd | Scalar    | 128  | 399.225 ns |  4.7633 ns |  8.02 |     251 B |
+| Naive            | Vector128 | 128  |  57.450 ns |  0.4304 ns |  1.15 |     212 B |
+| Generic          | Vector128 | 128  |  58.687 ns |  0.9553 ns |  1.18 |     212 B |
+| Recursive        | Vector128 | 128  | 416.948 ns | 11.0371 ns |  8.38 |     584 B |
+| Unrolled         | Vector128 | 128  |  52.548 ns |  1.3614 ns |  1.06 |     409 B |
+| FusedMultiplyAdd | Vector128 | 128  |  77.494 ns |  1.1821 ns |  1.56 |     217 B |
+| Naive            | Vector256 | 128  |  58.970 ns |  0.7746 ns |  1.18 |     212 B |
+| Generic          | Vector256 | 128  |  58.764 ns |  0.9418 ns |  1.18 |     212 B |
+| Recursive        | Vector256 | 128  | 409.922 ns |  9.1371 ns |  8.24 |     578 B |
+| Unrolled         | Vector256 | 128  |  52.989 ns |  1.1033 ns |  1.06 |     409 B |
+| FusedMultiplyAdd | Vector256 | 128  |  78.921 ns |  0.7225 ns |  1.59 |     217 B |
+*/
--- Vectorization.Benchmark/VectorizedDotProductVariantsBenchmark.cs
+++ Vectorization.Benchmark/VectorizedDotProductVariantsBenchmark.cs
@@ -5,32 +5,32 @@ namespace Vectorization.Benchmark;
 // Do not seal benchmark classes. Benchmark.Net subclasses them...
 public class VectorizedDotProductVariantsBenchmark
 {
-    private MyVector left, right;
+  private MyVector left, right;
 
-    [Params(3, 12, 128, 1521)]
-    public Int32 Size { get; set; }
+  [Params(3, 12, 128, 1521)]
+  public Int32 Size { get; set; }
 
-    [GlobalSetup]
-    public void Setup()
-    {
-        this.left = new MyVector(i => i, Size);
-        this.right = new MyVector(i => 1f / i, Size);
-    }
+  [GlobalSetup]
+  public void Setup()
+  {
+    this.left = new MyVector(i => i, Size);
+    this.right = new MyVector(i => 1f / i, Size);
+  }
 
-    [Benchmark(Baseline = true)]
-    public Single Scalar() => DotProduct.Scalar(this.left, this.right);
+  [Benchmark(Baseline = true)]
+  public Single Scalar() => DotProduct.Scalar(this.left, this.right);
 
-    [Benchmark]
-    public Single Vectorized() => DotProduct.Vectorized(this.left, this.right);
+  [Benchmark]
+  public Single Vectorized() => DotProduct.Vectorized(this.left, this.right);
 
-    [Benchmark]
-    public Single Vectorized128() => DotProduct.Vectorized(this.left, this.right);
+  [Benchmark]
+  public Single Vectorized128() => DotProduct.Vectorized(this.left, this.right);
 
-    [Benchmark]
-    public Single Vectorized256() => DotProduct.Vectorized(this.left, this.right);
+  [Benchmark]
+  public Single Vectorized256() => DotProduct.Vectorized(this.left, this.right);
 
-    [Benchmark]
-    public Single VectorizedRecursive() => DotProduct.RecursiveVectorized128(this.left, this.right);
+  [Benchmark]
+  public Single VectorizedRecursive() => DotProduct.RecursiveVectorized128(this.left, this.right);
 }
 
 /* Summary
@@ -68,3 +68,82 @@ BenchmarkDotNet v0.14.0, Windows 11 (10.0.22621.4037/22H2/2022Update/SunValley2)
 | Vectorized256       | 1521 |   219.252 ns |  2.8049 ns |  2.6237 ns |  0.35 |    0.01 |
 | VectorizedRecursive | 1521 | 3,929.271 ns | 76.7761 ns | 71.8164 ns |  6.34 |    0.15 |
 Summary */
+
+/* The same with varying SIMD capabilities (indicated by the 'Job' column)
+
+BenchmarkDotNet v0.14.0, Windows 11 (10.0.22621.4037/22H2/2022Update/SunValley2)
+13th Gen Intel Core i7-13850HX, 1 CPU, 28 logical and 20 physical cores
+.NET SDK 8.0.302
+  [Host]    : .NET 8.0.6 (8.0.624.26715), X64 RyuJIT AVX2
+  Scalar    : .NET 8.0.6 (8.0.624.26715), X64 RyuJIT
+  Vector128 : .NET 8.0.6 (8.0.624.26715), X64 RyuJIT AVX
+  Vector256 : .NET 8.0.6 (8.0.624.26715), X64 RyuJIT AVX2
+
+IterationTime=250ms  MaxIterationCount=20  WarmupCount=3  
+
+| Method              | Job       | Size | Mean          | StdDev      | Median        | Ratio | Code Size |
+|-------------------- |---------- |----- |--------------:|------------:|--------------:|------:|----------:|
+| Scalar              | Scalar    | 3    |      3.105 ns |   1.7265 ns |      1.781 ns |  1.31 |     209 B |
+| Vectorized          | Scalar    | 3    |      3.451 ns |   0.1198 ns |      3.449 ns |  1.46 |     617 B |
+| Vectorized128       | Scalar    | 3    |      4.990 ns |   2.1607 ns |      3.662 ns |  2.11 |     617 B |
+| Vectorized256       | Scalar    | 3    |      4.896 ns |   2.1401 ns |      3.517 ns |  2.07 |     617 B |
+| VectorizedRecursive | Scalar    | 3    |     30.712 ns |   0.1103 ns |     30.682 ns | 13.00 |   1,937 B |
+| Scalar              | Vector128 | 3    |      2.904 ns |   0.0661 ns |      2.925 ns |  1.23 |     212 B |
+| Vectorized          | Vector128 | 3    |      5.144 ns |   3.0703 ns |      3.256 ns |  2.18 |     423 B |
+| Vectorized128       | Vector128 | 3    |      3.121 ns |   0.1108 ns |      3.129 ns |  1.32 |     423 B |
+| Vectorized256       | Vector128 | 3    |      3.216 ns |   0.1131 ns |      3.213 ns |  1.36 |     423 B |
+| VectorizedRecursive | Vector128 | 3    |      4.829 ns |   2.2010 ns |      3.439 ns |  2.04 |     958 B |
+| Scalar              | Vector256 | 3    |      2.911 ns |   0.0698 ns |      2.914 ns |  1.23 |     212 B |
+| Vectorized          | Vector256 | 3    |      3.808 ns |   0.1337 ns |      3.775 ns |  1.61 |     436 B |
+| Vectorized128       | Vector256 | 3    |      3.760 ns |   0.1728 ns |      3.730 ns |  1.59 |     436 B |
+| Vectorized256       | Vector256 | 3    |      3.745 ns |   0.1546 ns |      3.694 ns |  1.58 |     436 B |
+| VectorizedRecursive | Vector256 | 3    |      3.238 ns |   0.1300 ns |      3.230 ns |  1.37 |     994 B |
+|                     |           |      |               |             |               |       |           |
+| Scalar              | Scalar    | 12   |      5.089 ns |   0.1487 ns |      5.114 ns |  1.00 |     209 B |
+| Vectorized          | Scalar    | 12   |     33.388 ns |   0.2741 ns |     33.321 ns |  6.57 |     652 B |
+| Vectorized128       | Scalar    | 12   |     33.662 ns |   0.1830 ns |     33.649 ns |  6.62 |     654 B |
+| Vectorized256       | Scalar    | 12   |     33.677 ns |   0.2091 ns |     33.596 ns |  6.62 |     654 B |
+| VectorizedRecursive | Scalar    | 12   |    173.463 ns |   1.9238 ns |    172.454 ns | 34.11 |   1,938 B |
+| Scalar              | Vector128 | 12   |      6.744 ns |   0.1461 ns |      6.750 ns |  1.33 |     212 B |
+| Vectorized          | Vector128 | 12   |      5.223 ns |   0.1097 ns |      5.248 ns |  1.03 |     417 B |
+| Vectorized128       | Vector128 | 12   |      5.247 ns |   0.1301 ns |      5.279 ns |  1.03 |     417 B |
+| Vectorized256       | Vector128 | 12   |      6.269 ns |   0.1337 ns |      6.295 ns |  1.23 |     417 B |
+| VectorizedRecursive | Vector128 | 12   |     28.416 ns |   0.3068 ns |     28.417 ns |  5.59 |     944 B |
+| Scalar              | Vector256 | 12   |      6.656 ns |   0.1284 ns |      6.659 ns |  1.31 |     212 B |
+| Vectorized          | Vector256 | 12   |      5.515 ns |   0.1781 ns |      5.563 ns |  1.08 |     432 B |
+| Vectorized128       | Vector256 | 12   |      5.372 ns |   0.1742 ns |      5.373 ns |  1.06 |     432 B |
+| Vectorized256       | Vector256 | 12   |      5.249 ns |   0.0718 ns |      5.249 ns |  1.03 |     434 B |
+| VectorizedRecursive | Vector256 | 12   |     29.080 ns |   0.6421 ns |     28.983 ns |  5.72 |     944 B |
+|                     |           |      |               |             |               |       |           |
+| Scalar              | Scalar    | 128  |     50.809 ns |   0.8930 ns |     50.567 ns |  1.00 |     209 B |
+| Vectorized          | Scalar    | 128  |    315.662 ns |   2.0052 ns |    315.505 ns |  6.21 |     652 B |
+| Vectorized128       | Scalar    | 128  |    314.036 ns |   1.3791 ns |    314.132 ns |  6.18 |     652 B |
+| Vectorized256       | Scalar    | 128  |    314.293 ns |   1.6915 ns |    313.936 ns |  6.19 |     652 B |
+| VectorizedRecursive | Scalar    | 128  |  1,490.347 ns |  11.5270 ns |  1,491.105 ns | 29.34 |   1,910 B |
+| Scalar              | Vector128 | 128  |     58.529 ns |   0.7070 ns |     58.330 ns |  1.15 |     212 B |
+| Vectorized          | Vector128 | 128  |     42.525 ns |   0.7428 ns |     42.404 ns |  0.84 |     419 B |
+| Vectorized128       | Vector128 | 128  |     42.600 ns |   0.7551 ns |     42.766 ns |  0.84 |     417 B |
+| Vectorized256       | Vector128 | 128  |     42.677 ns |   0.7867 ns |     42.941 ns |  0.84 |     417 B |
+| VectorizedRecursive | Vector128 | 128  |    251.558 ns |   5.5649 ns |    251.761 ns |  4.95 |     887 B |
+| Scalar              | Vector256 | 128  |     59.484 ns |   0.7958 ns |     59.499 ns |  1.17 |     212 B |
+| Vectorized          | Vector256 | 128  |     19.308 ns |   0.5198 ns |     19.341 ns |  0.38 |     434 B |
+| Vectorized128       | Vector256 | 128  |     19.055 ns |   0.5127 ns |     19.116 ns |  0.38 |     434 B |
+| Vectorized256       | Vector256 | 128  |     19.277 ns |   0.5805 ns |     19.387 ns |  0.38 |     434 B |
+| VectorizedRecursive | Vector256 | 128  |    250.940 ns |   4.6690 ns |    251.435 ns |  4.94 |     887 B |
+|                     |           |      |               |             |               |       |           |
+| Scalar              | Scalar    | 1521 |    607.197 ns |   8.9327 ns |    606.484 ns |  1.00 |     209 B |
+| Vectorized          | Scalar    | 1521 |  3,702.192 ns |  17.7244 ns |  3,693.266 ns |  6.10 |     659 B |
+| Vectorized128       | Scalar    | 1521 |  3,766.386 ns |  40.4839 ns |  3,760.896 ns |  6.20 |     659 B |
+| Vectorized256       | Scalar    | 1521 |  3,728.499 ns |  20.0454 ns |  3,722.482 ns |  6.14 |     659 B |
+| VectorizedRecursive | Scalar    | 1521 | 23,678.888 ns | 171.6468 ns | 23,644.865 ns | 39.00 |   1,908 B |
+| Scalar              | Vector128 | 1521 |    623.658 ns |   8.3922 ns |    620.175 ns |  1.03 |     212 B |
+| Vectorized          | Vector128 | 1521 |    430.486 ns |   6.5931 ns |    433.143 ns |  0.71 |     418 B |
+| Vectorized128       | Vector128 | 1521 |    437.890 ns |   3.5251 ns |    438.056 ns |  0.72 |     418 B |
+| Vectorized256       | Vector128 | 1521 |    435.634 ns |   7.0668 ns |    436.374 ns |  0.72 |     420 B |
+| VectorizedRecursive | Vector128 | 1521 |  4,117.747 ns |  69.5078 ns |  4,115.740 ns |  6.78 |     888 B |
+| Scalar              | Vector256 | 1521 |    634.120 ns |   9.4239 ns |    636.473 ns |  1.04 |     212 B |
+| Vectorized          | Vector256 | 1521 |    223.819 ns |   3.8289 ns |    224.696 ns |  0.37 |     435 B |
+| Vectorized128       | Vector256 | 1521 |    222.860 ns |   3.0391 ns |    223.252 ns |  0.37 |     435 B |
+| Vectorized256       | Vector256 | 1521 |    221.076 ns |   2.0913 ns |    221.415 ns |  0.36 |     435 B |
+| VectorizedRecursive | Vector256 | 1521 |  3,890.614 ns |  72.6268 ns |  3,868.519 ns |  6.41 |     888 B |
+*/
