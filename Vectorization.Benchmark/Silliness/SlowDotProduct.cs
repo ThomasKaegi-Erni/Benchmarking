c59e57ab42630b9a81c18d33396c538a8d01de91
Thomas Kägi <thomas.kaegi.tk1@contractors.roche.com>
2024-09-18 07:43:30 +0200

    Have slow dot product read better in profiling.

--- Vectorization.Benchmark/Silliness/SlowDotProduct.cs
+++ Vectorization.Benchmark/Silliness/SlowDotProduct.cs
@@ -13,22 +13,22 @@ public static class SlowDotProduct
             File.Delete(filename);
         }
         return Vector128.Sum(Dump(Recursive(in left, in right)));
+    }
 
-        static Vector128<Single> Recursive(in ReadOnlySpan<Single> l, in ReadOnlySpan<Single> r) => l.Length switch
-        {
-            0 => Vector128<Single>.Zero,
-            1 => Vector128.Create(l[0] * r[0], 0f, 0f, 0f),
-            2 => Vector128.Create(l[0] * r[0], l[1] * r[1], 0f, 0f),
-            3 => Vector128.Create(l[0], l[1], l[2], 0f) * Vector128.Create(r[0], r[1], r[2], 0f),
-            4 => Vector128.Create(l[0], l[1], l[2], l[3]) * Vector128.Create(r[0], r[1], r[2], r[3]),
-            var n => Recursive(l[..(n / 2)], r[..(n / 2)]) + Recursive(l[(n / 2)..], r[(n / 2)..])
-        };
+    private static Vector128<Single> Recursive(in ReadOnlySpan<Single> l, in ReadOnlySpan<Single> r) => l.Length switch
+    {
+        0 => Vector128<Single>.Zero,
+        1 => Vector128.Create(l[0] * r[0], 0f, 0f, 0f),
+        2 => Vector128.Create(l[0] * r[0], l[1] * r[1], 0f, 0f),
+        3 => Vector128.Create(l[0], l[1], l[2], 0f) * Vector128.Create(r[0], r[1], r[2], 0f),
+        4 => Vector128.Create(l[0], l[1], l[2], l[3]) * Vector128.Create(r[0], r[1], r[2], r[3]),
+        var n => Recursive(l[..(n / 2)], r[..(n / 2)]) + Recursive(l[(n / 2)..], r[(n / 2)..])
+    };
 
-        static Vector128<Single> Dump(Vector128<Single> vector)
-        {
-            using var text = File.AppendText(filename);
-            text.WriteLine(vector);
-            return vector;
-        }
+    private static Vector128<Single> Dump(Vector128<Single> vector)
+    {
+        using var text = File.AppendText(filename);
+        text.WriteLine(vector);
+        return vector;
     }
 }
